4318de78bcea84d7ec54a373b9e209b03f5bd0e1
vsoch <vsoch@users.noreply.github.com>
2023-06-20 17:17:27 -0600

    update to use custom dataclass added to argparse
    Signed-off-by: vsoch <vsoch@users.noreply.github.com>

--- snakemake_executor_flux/version.py
+++ snakemake_executor_flux/version.py
@@ -12,7 +12,10 @@ LICENSE = "LICENSE"
 
 # Since we assume wanting Singularity and lmod, we require spython and Jinja2
 
-INSTALL_REQUIRES = (("snakemake", {"min_version": None}),)
+INSTALL_REQUIRES = (
+    ("snakemake", {"min_version": None}),
+    ("argparse-dataclass", {"min_version": None}),
+)
 
 TESTS_REQUIRES = (("pytest", {"min_version": "4.6.2"}),)
 
